24bcd5f707c3449c2bdc36934c51dc9fc196b460
irsu75 <irinasukhanova01@gmail.com>
2023-03-31 14:24:00 -0400

    the engine specs has been added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        BasicStructure audi= new BasicStructure();
+        BasicStructure audi =new BasicStructure();
 
         audi.color="balck";
         audi.model="A3";
         audi.power="5000cc";
+
+        Engine X= new Engine();
+        X.cylinders="8";
+        X.size="1234cc";
+        X.weight="6453k";
+
+        Lights neon =new Lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="7675limin";
+
+        color col= new color();
+        col.colorLights="True";
+        col.colorTyre="false";
+        col.colorWindows="True";
 
     }
 }
